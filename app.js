fb76f7362036fa130936f4d9b692bc3cdb57a685
Hardik <hardikg2907@gmail.com>
2022-08-27 19:41:43 +0530

    keyboard inputs added and fixed issues

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const btnContainer = document.querySelector(".btn-container");
 
-const btnArr = [
-{id:"AC",inp:"AC"},
-{id:"plus",inp:"+"},
-{id:"minus",inp:"-"},
-{id:"multiply",inp:"x"},
-{id:"div",inp:"÷"},
-{id:"1",inp:"1"},
-{id:"2",inp:"2"},
-{id:"3",inp:"3"},
-{id:"4",inp:"4"},
-{id:"5",inp:"5"},
-{id:"6",inp:"6"},
-{id:"7",inp:"7"},
-{id:"8",inp:"8"},
-{id:"9",inp:"9"},
-{id:"0",inp:"0"},
-{id:"equals",inp:"="},
-];
+// const btnArr = [
+// {id:"AC",inp:"AC"},
+// {id:"plus",inp:"+"},
+// {id:"minus",inp:"-"},
+// {id:"multiply",inp:"x"},
+// {id:"div",inp:"÷"},
+// {id:"1",inp:"1"},
+// {id:"2",inp:"2"},
+// {id:"3",inp:"3"},
+// {id:"4",inp:"4"},
+// {id:"5",inp:"5"},
+// {id:"6",inp:"6"},
+// {id:"7",inp:"7"},
+// {id:"8",inp:"8"},
+// {id:"9",inp:"9"},
+// {id:"0",inp:"0"},
+// {id:"equals",inp:"="},
+// ];
 
 // document.addEventListener("DOMContentLoaded", function() {
 //     for(i=0;i<btnArr.length;i++){
@@ -30,6 +30,12 @@ const ans = document.querySelector('.answer');
 const buttons = document.querySelectorAll('.button');
 var result = 0;
 
+function ifac(op){
+    if(ans.textContent.length==1 && ans.textContent == '0' && [1,2,3,4,5,6,7,8,9,0].includes(parseInt(op)))
+    {
+        ans.textContent = "";
+    }
+}
 
 // function ifop(op){
 //     if(ans.textContent[ans.textContent.length-1]=='+' || ans.textContent[ans.textContent.length-1]=='-' || ans.textContent[ans.textContent.length-1]=='x' || ans.textContent[ans.textContent.length-1]=='÷'){
@@ -45,6 +51,7 @@ buttons.forEach(
         btn.addEventListener('click',function(){
             const op = btn.textContent;
             console.log(op);
+            ifac(op);
 
             switch(op){
                 case 'AC': ans.textContent = 0;
@@ -52,7 +59,6 @@ buttons.forEach(
                 break;
 
                 case 'x':  ans.textContent += '*';
-                // ifop(op);
                 break;
 
                 case '=': result = eval(ans.textContent);
@@ -70,5 +76,48 @@ buttons.forEach(
         })
     }
 )
+
+// keyboard inputs
+
+document.addEventListener('keypress', event =>{
+    var kop = event.key;
+    console.log(kop);
+    ifac(kop);
+    switch(kop){
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '0': ans.textContent += kop;
+        break;
+
+        case 'Delete': let a = ans.textContent.slice(0,ans.textContent.length-1);
+        ans.textContent = a;
+        break;
+
+        case '=':
+        case 'Enter': result = eval(ans.textContent);
+            console.log(result);
+            ans.textContent = result;
+            break;
+
+        case 'c': ans.textContent = 0;
+           result = 0;
+           break;
+
+        default: ans.textContent = ans.textContent;
+    }
+})
+
+
 
 console.log("It's on!!");
